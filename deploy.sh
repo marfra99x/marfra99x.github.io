25949f191dcaf65d7aa97e89d718dd222830466e
marfra99x <marfra99x@gmail.com>
2022-11-21 23:57:31 +0100

    changed deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ cd dist
 git init 
 git add -A
 git commit -m 'Deploy'
-git push -f git@github.com:marfra99x/marfra99x.github.io.git master/gh-pages
+# git push -f git@github.com:marfra99x/marfra99x.github.io.git master/gh-pages
+
+git push -f git@github.com:marfra99x/marfra99x.github.io.git master
 
 cd -
