02209f876e5d0b1d26bc0f15f57954c9c9c318ce
marfra99x <marfra99x@gmail.com>
2022-11-21 23:36:31 +0100

    created

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh 
+
+set -e
+
+npm run build
+
+cd dist
+
+git init 
+git add -A
+git commit -m 'Deploy'
+git push -f git@github.com:marfra99x/marfra99x.git master/gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/marfra99x/'
+}
